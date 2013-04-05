9d6d41051e7ca01f05c75c0c15e76ca73a6fa1ba
Yuri Parsons <yparsons@mog.com>
2013-04-05 13:29:48 +0200

    fix

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,19 @@
 require.config({
-	"paths": {
-		"jquery": "public/scripts/vendor/jquery/jquery",
-		"underscore": "vendor/underscore-amd/underscore",
-		"backbone": "public/scripts/vendor/backbone",
-		"marionette": "public/scripts/vendor/marionette/lib/backbone.marionette",
-		"backbone-amd": "public/scripts/vendor/backbone-amd",
-		"underscore-amd": "public/scripts/vendor/underscore-amd"
+	"paths": {},
+	"shim": {
+		"backbone": {
+			"deps": [
+				"underscore",
+				"jquery"
+			],
+			"exports": "Backbone"
+		},
+		"underscore": {
+			"exports": "_"
+		}
 	}
 });
 
-require(['views/app'], function(AppView){
-	 new AppView;
-	});
-	
+require(['views/app'], function(AppView) {
+	new AppView;
+});
