7615dc882c8aa438583ec539c9c90cc762c0f9a4
Yuri Parsons <yparsons@mog.com>
2013-04-17 15:45:34 +0200

    make it look a tiny bit nicer

--- public/scripts/views/app.js
+++ public/scripts/views/app.js
@@ -6,7 +6,8 @@ define(['backbone'], function(Backbone) {
 		},
 		render: function(){
 			console.log( 'render' );
-			this.$el.html("<p>Hello World</p>");
+			var template = '<div class="row"><div class="col-span-12"><h1>Hello World</h1></div></div>'
+			this.$el.html(template);
 			return this
 		}
 	});
